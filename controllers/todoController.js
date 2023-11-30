a890f557b94455b53bb80b7fd312429e392d9371
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-11-30 15:40:43 +0700

    UPDATE: todo tempalte

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,18 +1,22 @@
 import supabase from "../lib/supabase.js";
 
 export const createTodo = async (req, res) => {
-  const { name, amount, date, group_id } = req.body;
-  const { data, error } = await supabase.from("todos").insert({
+  const { name, amount, checked = false, date, group_id } = req.body;
+  const { data: todo, error } = await supabase.from("todos").insert({
     name,
     amount,
-    checked: false,
+    checked,
     date,
     group_id,
   });
-  res.json({ data, error });
+  res.json({ todo, error });
 };
 
 export const getTodos = async (req, res) => {
-  const { data, error } = await supabase.from("todos").select();
-  res.json({ data, error });
+  const { data: todos, error } = await supabase.from("todos").select();
+  res.json({ todos, error });
 };
+
+export const updateTodo = async (req, res) => {};
+
+export const deleteTodo = async (req, res) => {};
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createTodo, getTodos } from "../controllers/todoController.js";
+import {
+  createTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/todoController.js";
 
 const todoRouter = express.Router();
 
 todoRouter.post("/", createTodo);
 todoRouter.get("/", getTodos);
+todoRouter.put("/:id", updateTodo);
+todoRouter.delete("/:id", deleteTodo);
 
 export default todoRouter;
