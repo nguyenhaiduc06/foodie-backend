dd24721c6a36b8eeaa3616ca3252e9483c5c0f29
doccocaubaii <102582078+doccocaubaii@users.noreply.github.com>
2023-12-08 15:47:29 +0700

    them group va member khi signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import supabase from "../lib/supabase.js";
 import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt";
 import dotenv from 'dotenv';
+
 dotenv.config();
 export const signUp = async (req, res) => {
     // validate
@@ -10,12 +11,11 @@ export const signUp = async (req, res) => {
     if (password == null || password == "") return res.status(400).json({error: "password là bắt buộc"});
     if (name == null || name == "") return res.status(400).json({error: "name là bắt buộc"});
 
-    const {data, error2} = await supabase
+    const {data, error : error2} = await supabase
         .from('users')
         .select()
         .eq('username', username);
     if (data != null && data.length) return res.status(400).json({error: "username đã tồn tại"});
-
 
     let hashedPassword;
     try {
@@ -26,44 +26,58 @@ export const signUp = async (req, res) => {
     }
     // create a new user
     let newUser = {
-        created_at: new Date(),
+        created_at: new Date().toISOString(),
         name: name,
         username: username,
         password: hashedPassword,
         avatar_url: avatarUrl
     }
-    const {error} = await supabase.from("users").insert(newUser);
-    // Create token
-    const token = createJwt({username: newUser.username, name: newUser.name});
+    let {data:x, error: y} =  await supabase.from("groups").select().eq('name', "abcd");
+    let {data : user, error} = await supabase.from("users").insert(newUser).select().single();
+    delete user.password;
+    let {data: group, error: errorGroup} = await supabase.from("groups").insert({
+            created_at: new Date().toISOString(),
+            name: username + "Group",
+        }
+    ).select().single();
+    let {data : member, error : errorMember} = await supabase.from("members").insert(
+    {
+        created_at : new Date().toISOString(),
+        user_id : user.id,
+        group_id : group.id
+    }).select().single();
+
+    const token = createJwt({user});
     res.json({token});
 };
 
 export const signIn = async (req, res) => {
     const {username, password} = req.body;
-  if (username == null || username == "") return res.status(400).json({error: "username là bắt buộc"});
-  if (password == null || password == "") return res.status(400).json({error: "password là bắt buộc"});
+    if (username == null || username == "") return res.status(400).json({error: "username là bắt buộc"});
+    if (password == null || password == "") return res.status(400).json({error: "password là bắt buộc"});
 
-  const {data, error} = await supabase
-      .from('users')
-      .select()
-      .eq('username', username);
-  if (data == null || data.length == 0) return res.status(400).json({error: "username không tồn tại"});
-  const user = data[0];
-  try {
-    const result = bcrypt.compareSync(password, user.password);
-    if (!result) return res.status(400).json({error: "Mật khẩu không khớp"});
-  } catch (error) {
-    console.error('Lỗi so sánh mật khẩu:', error);
-    return res.status(500).json({error: 'Không thể so sánh mật khẩu'});
-  }
-  const token = createJwt({username: user.username, name: user.name});
-  res.json({token});
+    const {data, error} = await supabase
+        .from('users')
+        .select()
+        .eq('username', username);
+    if (data == null || data.length == 0) return res.status(400).json({error: "username không tồn tại"});
+    const user = data[0];
+    try {
+        const result = bcrypt.compareSync(password, user.password);
+        if (!result) return res.status(400).json({error: "Mật khẩu không khớp"});
+    } catch (error) {
+        console.error('Lỗi so sánh mật khẩu:', error);
+        return res.status(500).json({error: 'Không thể so sánh mật khẩu'});
+    }
+    delete user.password;
+    const token = createJwt({user});
+    res.json({token});
 };
 
 export const getUser = async (req, res) => {
     console.log(req);
     const {id} = req.params;
-    if (id  == null) return res.status(400).json({error: "id là bắt buộc"});
+    if (id == null) return res.status(400).json({error: "id là bắt buộc"});
     const {data, error2} = await supabase
         .from('users')
         .select()
@@ -76,14 +90,14 @@ export const getUser = async (req, res) => {
     res.json({data});
 };
 
-const createJwt = function(payload) {
-  const secret_key = process.env.SECRET_KEY;
-  const token = jwt.sign(
-      payload,
-      secret_key,
-      {
-        expiresIn: "2h",
-      }
-  );
-  return token;
+const createJwt = function (payload) {
+    const secret_key = process.env.SECRET_KEY;
+    const token = jwt.sign(
+        payload,
+        secret_key,
+        {
+            expiresIn: "2h",
+        }
+    );
+    return token;
 }
