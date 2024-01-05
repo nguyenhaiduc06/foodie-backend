921862f6afa202ed45759d02e8d02a2e23c12d93
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2024-01-05 13:29:02 +0700

    UPDATE: todo controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,54 @@
 import supabase from "../lib/supabase.js";
+import dayjs from "dayjs";
 
 export const createTodo = async (req, res) => {
-  const { name, amount, checked = false, date, group_id } = req.body;
-  const { data: todo, error } = await supabase.from("todos").insert({
-    name,
-    amount,
-    checked,
-    date,
-    group_id,
-  });
-  res.json({ todo, error });
+  const { group_id, date, name, amount } = req.body;
+  if (!group_id || !date || !name || !amount) return;
+  const { data, error } = await supabase
+    .from("todos")
+    .insert({
+      group_id,
+      name,
+      amount,
+      date,
+    })
+    .select()
+    .single();
+  res.json({ data, error });
 };
 
 export const getTodos = async (req, res) => {
-  const { data: todos, error } = await supabase.from("todos").select();
-  res.json({ todos, error });
+  const { group_id, date } = req.query;
+  const startOfDate = dayjs(date).startOf("date").toISOString();
+  const endOfDate = dayjs(date).endOf("date").toISOString();
+  const { data, error } = await supabase
+    .from("todos")
+    .select("*")
+    .lte("date", endOfDate)
+    .gte("date", startOfDate)
+    .eq("group_id", group_id);
+  res.json({ data, error });
 };
 
 export const updateTodo = async (req, res) => {
-  console.log("abc");
+  const { id } = req.params;
+  const { name, amount, checked, date } = req.body;
+  const { data, error } = await supabase
+    .from("todos")
+    .update({
+      name,
+      amount,
+      checked,
+      date,
+    })
+    .eq("id", id)
+    .select()
+    .single();
+  res.json({ data, error });
 };
 
-export const deleteTodo = async (req, res) => {};
+export const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+  const { error } = await supabase.from("todos").delete().eq("id", id);
+  return { error };
+};
