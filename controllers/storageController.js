ea3f3db66d8c995f7a4235198bf9e16a2e4bce35
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2024-01-05 14:35:28 +0700

    UPDATE: schedule notification for storage

--- controllers/storageController.js
+++ controllers/storageController.js
@@ -1,13 +1,27 @@
 import supabase from "../lib/supabase.js";
+import { scheduleJob, rescheduleJob } from "node-schedule";
+import { sendPushNotification } from "../services/notification.js";
+import dayjs from "dayjs";
 
 export const createStorage = async (req, res) => {
-  const { group_id, name, amount, image_url, stored_at, expire_date } =
+  const { group_id, name, amount, image_url, stored_in, expire_date } =
     req.body;
   const { data, error } = await supabase
     .from("storages")
-    .insert({ group_id, name, amount, stored_at, expire_date, image_url })
+    .insert({ group_id, name, amount, stored_in, expire_date, image_url })
     .select()
     .single();
+
+  // Schedule a job to send notification
+  const jobId = data.id.toString(); // jobId must be a string
+  const scheduledDate = dayjs().add(5, "seconds").toDate();
+  const push_token = "ExponentPushToken[vvSWZjKZRF0j8-KL35kPqP]";
+  const title = "Thực phẩm sắp hết hạn";
+  const body = `${name} trong ${stored_in} sẽ hết hạn trong 3 ngày tới`;
+  scheduleJob(jobId, scheduledDate, () =>
+    sendPushNotification(push_token, title, body)
+  );
+
   res.json({ data, error });
 };
 
@@ -26,13 +40,19 @@ export const getStorageDetails = async (req, res) => {
 
 export const updateStorage = async (req, res) => {
   const { id } = req.params;
-  const { name, amount, image_url, stored_at, expire_date } = req.body;
+  const { name, amount, image_url, stored_in, expire_date } = req.body;
   const { data, error } = await supabase
     .from("storages")
-    .update({ name, amount, stored_at, expire_date, image_url })
+    .update({ name, amount, stored_in, expire_date, image_url })
     .eq("id", id)
     .select()
     .single();
+
+  // Reschedule send notification job
+  const jobId = id.toString();
+  const scheduledDate = dayjs(expire_date).add(5, "seconds");
+  rescheduleJob(jobId, scheduledDate);
+
   res.json({ data, error });
 };
 
@@ -41,3 +61,5 @@ export const deleteStorage = async (req, res) => {
   const { error } = await supabase.from("storages").delete().eq("id", id);
   res.json({ error });
 };
+
+const scheduleSendNotificationJob = () => {};
---
+++ services/notification.js
@@ -0,0 +1,24 @@
+import { Expo } from "expo-server-sdk";
+
+let expo = new Expo();
+
+export const sendPushNotification = async (push_token, title, body) => {
+  const messages = [
+    {
+      to: push_token,
+      title,
+      body,
+    },
+  ];
+  const chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
